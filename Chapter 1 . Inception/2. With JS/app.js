9173e504232dc7b19808ce5344da16fc0a549eae
Dhawal Pandya <95483771+dhawal-pandya@users.noreply.github.com>
2023-02-17 15:13:07 +0530

    del

--- Chapter 1 . Inception/2. With JS/app.js
+++
@@ -1,7 +0,0 @@
-const h1 = document.createElement('h1');
-
-h1.innerHTML = 'Namaste World';
-
-const root = document.getElementById('root');
-
-root.appendChild(h1);
--- Chapter 1 . Inception/3. With React/app.js
+++
@@ -1,5 +0,0 @@
-const h1 = React.createElement('h1', { id: 'title' }, 'Namaste World');
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(h1);
