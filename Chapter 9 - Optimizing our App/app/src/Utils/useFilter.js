c9ca45e260ec79a422cb0acd42f8572acbc80308
Dhawal Pandya <95483771+dhawal-pandya@users.noreply.github.com>
2023-02-17 15:16:20 +0530

    del

--- Chapter 9 - Optimizing our App/app/src/Utils/useFilter.js
+++
@@ -1,9 +0,0 @@
-const useFilter = (search, resData) =>
-  search !== '' &&
-  resData.filter(
-    (res, search) =>
-      res.data.name.toLowerCase().includes(search.toLowerCase()) ||
-      res.data.cuisines.join('').toLowerCase().includes(search.toLowerCase())
-  );
-
-export default useFilter;
